27c537735be0f66d47673689d9640da6397e2847
Djamel <pruevadjamel2020@hotmail.com>
2023-09-02 04:02:57 +0000

    holaaaa2

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Define la URL base de la API en la nube
-const BASE_URL = 'https://bckendnuevo.onrender.com'; // Reemplaza con la URL real de tu API
+const BASE_URL = "mongodb://nombredjamel:contrasenadjamel@imobiliaria-shard-00-00.ic6yw.mongodb.net:27017,imobiliaria-shard-00-01.ic6yw.mongodb.net:27017,imobiliaria-shard-00-02.ic6yw.mongodb.net:27017/djamel04?ssl=true&replicaSet=atlas-5vh19o-shard-0&authSource=admin&retryWrites=true&w=majority" 
 
 export const getDataAPI = async (url, token) => {
     const res = await axios.get(`${BASE_URL}/api/${url}`, {
