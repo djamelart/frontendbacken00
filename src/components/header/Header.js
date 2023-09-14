8c1d74d6552987d9fc9a55420bc8405e0979f183
Djamel <pruevadjamel2020@hotmail.com>
2023-09-14 04:29:39 +0000

    header33333

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,40 +1,52 @@
-import React ,{useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import Menu from './Menu'
-import { useHistory } from 'react-router-dom'
-import { useSelector  } from 'react-redux'
-import { useTranslation } from 'react-i18next'
-const Header = () => {
-    const {  languagee } = useSelector(state => state)
-  const { user } = useSelector(state => state.auth);
-    
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Menu from './Menu';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
-    const history = useHistory()
+const Header = () => {
+  const { languagee } = useSelector((state) => state);
+  const { user } = useSelector((state) => state.auth);
+
+  const history = useHistory();
+  const { t } = useTranslation();
+
+  // Crear un estado local para controlar la visibilidad del header
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
   useEffect(() => {
-      
-    if  (user.role ="bloqueado") {
-      
-      history.push('/pages/bloqueos');
+    // Verificar si el usuario tiene el rol "bloqueado"
+    if (user.role === "bloqueado") {
+      // Redirigir al usuario bloqueado a la página de bloqueos
+      history.push('/bloqueos');
+
+      // Ocultar el header
+      setIsHeaderVisible(false);
+    } else {
+      // Mostrar el header para otros usuarios
+      setIsHeaderVisible(true);
     }
   }, [user, history]);
-    const { t } = useTranslation();
-    return (
-        <div className="header bg-light">
-            <nav className="navbar navbar-expand-lg navbar-light 
-            bg-light justify-content-between align-middle">
 
-                <Link to="/" className="logo">
-                    <h1 className="navbar-brand text-uppercase p-0 m-0"
-                    onClick={() => window.scrollTo({top: 0})}>
-                        {t('Tassili', { lng: languagee.language })}
-                    </h1>
-                </Link>
- 
+  // Renderizar el header condicionalmente
+  return isHeaderVisible ? (
+    <div className="header bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
+        <Link to="/" className="logo">
+          <h1
+            className="navbar-brand text-uppercase p-0 m-0"
+            onClick={() => window.scrollTo({ top: 0 })}
+          >
+            {t('Tassili', { lng: languagee.language })}
+          </h1>
+        </Link>
 
-                <Menu />
-            </nav>
-        </div>
-    )
-}
+        <Menu />
+      </nav>
+    </div>
+  ) : null;
+};
 
-export default Header
+export default Header;
+
